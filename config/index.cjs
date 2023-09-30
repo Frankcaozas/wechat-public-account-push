21002f23bf671e94d4efa63755e8303410db80b9
frankcao <1666930632@qq.com>
2023-09-30 23:18:55 +0800

    feat: config

--- config/index.cjs
+++ config/index.cjs
@@ -53,7 +53,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oEjM86_Z3i1Cvb5HcM1rts9zlYcw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'rEdGkuXcBVO5Q5Mcq9hd6W8VAbmXXB2Bub2HPd75y68',
+      useTemplateId: 'wYlEO9XA0fcs2gtyY5_vS9c8VMn5F6RXZF4ZL-hvKnI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
