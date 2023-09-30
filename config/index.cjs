4aac38280e30c80d537b4388597e254c6f263fa3
frankcao <90302102+Frankcaozas@users.noreply.github.com>
2023-09-30 22:19:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -47,6 +47,36 @@ const USER_CONFIG = {
         // { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
+    {
+      // 想要发送的人的名字
+      name: '宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oEjM861s1PlFxHPVVgAu893qpPjk',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'NrEjafWprosgEAis4RokrwvlMehZH9SpXMR92687mGI',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '12-27',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '2002', date: '03-15',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        // {
+        //   type: '生日', name: '李四', year: '1996', date: '09-31',
+        // },
+        {
+          type: '节日', name: '相识纪念日', year: '2022', date: '09-03',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-01-01' },
+        // 结婚纪念日
+        // { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
   ],
 
 
