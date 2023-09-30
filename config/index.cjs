d7e7fffb3c8fcfd87d2d61ee1fc15e662c185582
frankcao <90302102+Frankcaozas@users.noreply.github.com>
2023-09-30 22:55:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -53,7 +53,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oEjM86_Z3i1Cvb5HcM1rts9zlYcw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'u5cc7_fW-Sbcryxkyd6T3k2k93GAv8WZXKuYB-CHGUk'
+      useTemplateId: 'u5cc7_fW-Sbcryxkyd6T3k2k93GAv8WZXKuYB-CHGUk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
