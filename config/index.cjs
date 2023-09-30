bd9938a6eb0ae2b183d314d78a88def6e601728f
frankcao <90302102+Frankcaozas@users.noreply.github.com>
2023-09-30 22:39:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oEjM861s1PlFxHPVVgAu893qpPjk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'NrEjafWprosgEAis4RokrwvlMehZH9SpXMR92687mGI',
+      useTemplateId: 'cCSVxa8LAOsvoFCPUzRxa6AXwpTdsvYvuruvqI1l8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -53,7 +53,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oEjM86_Z3i1Cvb5HcM1rts9zlYcw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'NrEjafWprosgEAis4RokrwvlMehZH9SpXMR92687mGI',
+      useTemplateId: 'cCSVxa8LAOsvoFCPUzRxa6AXwpTdsvYvuruvqI1l8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
