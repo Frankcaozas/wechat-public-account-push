0c5dabb37fd278f89e4971dcb4ec3953aa740ef7
frankcao <90302102+Frankcaozas@users.noreply.github.com>
2023-10-03 22:49:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -81,7 +81,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'w5VlmVU2v0p9arjQ8vR6iLGXLRwbfobLPGL2sIqaCz8',
 
   CALLBACK_USERS: [
     {
